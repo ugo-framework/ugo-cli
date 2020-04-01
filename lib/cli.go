409c2db40b1a5d545eb91f4702357f034aca9a9c
ayanrocks <ayanb1999@gmail.com>
2020-04-02 01:45:37 +0530

    Adding flag verbose

--- lib/cli.go
+++ lib/cli.go
@@ -1,24 +1,32 @@
 package cli
 
 import (
-	"fmt"
-	"github.com/spf13/cobra"
-	"os"
+  "fmt"
+  "github.com/spf13/cobra"
+  "os"
+)
+
+var (
+  Verbose bool
 )
 
 var rootCmd = &cobra.Command{
-	Use:   "hugo",
-	Short: "Hugo is a very fast static site generator",
-	Long: `A Fast and Flexible Static Site Generator built with
-                love by spf13 and friends in Go.
-                Complete documentation is available at http://hugo.spf13.com`,
+  Use:   "hugo",
+  Short: "Hugo is a very fast static site generator",
+  Long: `A Fast and Flexible Static Site Generator built with
+        love by spf13 and friends in Go.
+        Complete documentation is available at http://hugo.spf13.com`,
+}
+
+func init() {
+  rootCmd.PersistentFlags().BoolVarP(&Verbose, "verbose", "v", false, "verbose output")
 }
 
 func Execute() error {
-	if err := rootCmd.Execute(); err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-		return err
-	}
-	return nil
+  if err := rootCmd.Execute(); err != nil {
+    fmt.Println(err)
+    os.Exit(1)
+    return err
+  }
+  return nil
 }
