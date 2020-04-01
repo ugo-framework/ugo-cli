bdda457372beee660738092af159a0e3056f99ee
ayanrocks <ayanb1999@gmail.com>
2020-04-02 00:18:43 +0530

    Added LICENSE, COBRA

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/ugo-framework/ugo-cli
 
 go 1.14
+
+require github.com/spf13/cobra v0.0.7 // indirect
--- lib/cli.go
+++ lib/cli.go
@@ -1,2 +1,27 @@
 package cli
 
+import (
+	"fmt"
+	"github.com/spf13/cobra"
+	"os"
+)
+
+//var rootCmd *cobra.Command
+
+var rootCmd = &cobra.Command{
+	Use:   "hugo",
+	Short: "Hugo is a very fast static site generator",
+	Long: `A Fast and Flexible Static Site Generator built with
+                love by spf13 and friends in Go.
+                Complete documentation is available at http://hugo.spf13.com`,
+	Run: func(cmd *cobra.Command, args []string) {
+		// Do Stuff Here
+		fmt.Println("HUGGGGO")
+	},
+}
+
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
